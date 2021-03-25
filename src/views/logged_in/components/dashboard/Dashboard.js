083b03a02a181bf49e57da08ba992523e2459674
kanekotic <me@kanekotic.com>
2021-03-25 09:27:29 +0100

    Alvaro | improve logged in ui dashboard

--- src/views/logged_in/components/dashboard/Dashboard.js
+++ src/views/logged_in/components/dashboard/Dashboard.js
@@ -1,17 +1,23 @@
 import React, { Fragment, useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { Typography, Box, Button } from "@material-ui/core";
+import { Typography, Box, Button, withStyles } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import Toggles from "./Toggles";
 import AccountInformationArea from "./AccountInformationArea";
 
+const styles = theme => ({
+  ProjectBox: {
+    marginBottom: theme.spacing(1),
+  }
+})
 function Dashboard(props) {
   const {
     user,
     selectDashboard,
     pushMessageToSnackbar,
     projectUpsert,
-    toggleAccountActivation
+    toggleAccountActivation,
+    classes
   } = props;
 
   const [projects, setProjects] = useState([]);
@@ -27,10 +33,10 @@ function Dashboard(props) {
       isAccountActivated={false}
       toggleAccountActivation={toggleAccountActivation}
     />
-      <Box mt={4}>
-        <Typography variant="subtitle1" gutterBottom>
+      <Box display="flex" flexDirection="row" justifyContent="flex-end" mt={4} className={classes.ProjectBox}>
+      <Box width="100%" ><Typography variant="subtitle1" gutterBottom>
           Your Projects
-        </Typography>
+        </Typography></Box>
         <Button 
           variant="contained" 
           color="primary" 
@@ -62,6 +68,7 @@ Dashboard.propTypes = {
   isAccountActivated: PropTypes.bool.isRequired,
   toggleAccountActivation: PropTypes.func,
   projectUpsert: PropTypes.func,
+  classes: PropTypes.object.isRequired,
 };
 
-export default Dashboard;
+export default withStyles(styles, { withTheme: true })(Dashboard);
